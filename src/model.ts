d8edc7c8150d0ae87d7d6177e7f11881d4c4af3a
Ihor Stepanov <igor.stepanov2000@gmail.com>
2024-09-24 20:29:43 +0300

    add text to the inputs and optimize tensors preparation

--- src/model.ts
+++ src/model.ts
@@ -18,42 +18,20 @@ export class Model {
     const num_tokens = batch.inputsIds[0].length;
     const num_spans = batch.spanIdxs[0].length;
 
-    const convertToBool = (arr: any[]): any[] => {
-      return arr.map((subArr) => subArr.map((val: any) => !!val));
-    };
-
-    const blankConvert = (arr: any[]): any[] => {
-      return arr.map((val) => (isNaN(Number(val)) ? 0 : Math.floor(Number(val))));
-    };
-
-    const convertToInt = (arr: any[]): any[] => {
-      return arr.map((subArr) =>
-        subArr.map((val: any) => (isNaN(Number(val)) ? 0 : Math.floor(Number(val))))
-      );
-    };
-
-    const convertSpanIdx = (arr: any[]): any[] => {
-      return arr.flatMap((subArr) =>
-        subArr.flatMap((pair: any) => pair.map((val: any) => (isNaN(Number(val)) ? 0 : Math.floor(Number(val)))))
-      );
-    };
-
     const createTensor = (
       data: any[],
       shape: number[],
-      conversionFunc: (arr: any[]) => any[] = convertToInt,
       tensorType: any = "int64"
     ): ort.Tensor => {
-      const convertedData = conversionFunc(data);
-      return new this.onnxWrapper.ort.Tensor(tensorType, convertedData.flat(Infinity), shape);
+      return new this.onnxWrapper.ort.Tensor(tensorType, data.flat(Infinity), shape);
     };
 
     let input_ids = createTensor(batch.inputsIds, [batch_size, num_tokens]);
-    let attention_mask = createTensor(batch.attentionMasks, [batch_size, num_tokens], convertToBool); // NOTE: why convert to bool but type is not bool?
+    let attention_mask = createTensor(batch.attentionMasks, [batch_size, num_tokens]); // NOTE: why convert to bool but type is not bool?
     let words_mask = createTensor(batch.wordsMasks, [batch_size, num_tokens]);
-    let text_lengths = createTensor(batch.textLengths, [batch_size, 1], blankConvert);
-    let span_idx = createTensor(batch.spanIdxs, [batch_size, num_spans, 2], convertSpanIdx);
-    let span_mask = createTensor(batch.spanMasks, [batch_size, num_spans], convertToBool, "bool");
+    let text_lengths = createTensor(batch.textLengths, [batch_size, 1]);
+    let span_idx = createTensor(batch.spanIdxs, [batch_size, num_spans, 2]);
+    let span_mask = createTensor(batch.spanMasks, [batch_size, num_spans], "bool");
 
     const feeds = {
       input_ids: input_ids,
@@ -84,12 +62,14 @@ export class Model {
     const inputLength = Math.max(...batch.textLengths);
     const maxWidth = this.config.max_width;
     const numEntities = entities.length;
-
+    const batchIds = Array.from({ length: batchSize }, (_, i) => i);
     const decodedSpans = this.decoder.decode(
       batchSize,
       inputLength,
       maxWidth,
       numEntities,
+      texts,
+      batchIds,
       batch.batchWordsStartIdx,
       batch.batchWordsEndIdx,
       batch.idToClass,
@@ -147,6 +127,7 @@ export class Model {
         let currBatchTokens = batchTokens.slice(start, end);
         let currBatchWordsStartIdx = batchWordsStartIdx.slice(start, end);
         let currBatchWordsEndIdx = batchWordsEndIdx.slice(start, end);
+        let currBatchIds = batchIds.slice(start, end);
 
         let [inputTokens, textLengths, promptLengths] = this.processor.prepareTextInputs(currBatchTokens, entities);
 
@@ -190,6 +171,8 @@ export class Model {
           inputLength,
           maxWidth,
           numEntities,
+          texts,
+          currBatchIds,
           batch.batchWordsStartIdx,
           batch.batchWordsEndIdx,
           batch.idToClass,
