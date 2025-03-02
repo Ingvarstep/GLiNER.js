4528498b0eedb68977cc3fafdee5406e11698f4d
juliankolbe <juliankolbe@live.com>
2025-03-02 17:55:36 +0100

    add changeset and interfaces

---
+++ .changeset/sad-badgers-pick.md
@@ -0,0 +1,5 @@
+---
+"gliner": patch
+---
+
+Fixate onnx versions for stability
--- src/node/index.ts
+++ src/node/index.ts
@@ -1,2 +1,14 @@
 export * from "./Gliner";
 export { WhitespaceTokenSplitter } from "../lib/processor";
+// Export types from interfaces
+export type {
+  IEntityResult,
+  InferenceResultSingle,
+  InferenceResultMultiple,
+  IInference,
+  InitConfig,
+  RawInferenceResult,
+  IONNXNodeSettings,
+  ExecutionProvider,
+  ITransformersSettings,
+} from "../interfaces";
